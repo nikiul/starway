5bb94344f128fbe3665d2f9c17c2ad2d698a789b
nikiul <nikulans@gmail.com>
2021-03-30 22:44:33 +0300

    added 768 responsive

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 // Бургер
-
-var mobileMenuBtn = document.querySelector(".control__burger");
+var controlBurger = document.querySelector(".control__burger");
 var menuIcon = document.querySelector(".burger__icon");
-// var mobileMenu = document.querySelector(".header");
-// var headerMobileBtn = document.querySelector(".header__menu-closet");
-// var bodyLock = document.querySelector("body");
+var mobileBurger = document.querySelector(".mobile__burger");
+// var mobileIcon = document.querySelector(".mobile__burger-icon");
+var mobileMenu = document.querySelector(".desctop-menu");
+var bodyLock = document.querySelector("body");
 
-mobileMenuBtn.addEventListener("click", function(){
-    menuIcon.classList.toggle('burger__icon-active');
-    // mobileMenu.classList.add('header__mobile-nav--active');
-    // bodyLock.classList.add("body__lock");
+controlBurger.addEventListener("click", function(){
+    menuIcon.classList.toggle('burger__icon--active');
+    mobileMenu.classList.toggle('desctop-menu--active');
+    bodyLock.classList.toggle("body__lock");
 })
 
-// headerMobileBtn.addEventListener("click", function(){
-//     menuIcon.classList.remove('menu-icon-active');
-//     mobileMenu.classList.remove("header__mobile-nav--active");
-//     bodyLock.classList.remove("body__lock");
-// })
+mobileBurger.addEventListener("click", function(){
+    menuIcon.classList.toggle('burger__icon--active');
+    mobileMenu.classList.toggle('desctop-menu--active');
+    bodyLock.classList.toggle("body__lock");
+})
+
+// Карусель
+$('.products__tablet').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
